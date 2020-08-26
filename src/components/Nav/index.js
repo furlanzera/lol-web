b655a69d659110de4a164e1684723a5c7448b7cb
Lucas Furlan <lucasfurlan2000@hotmail.com>
2020-08-26 17:58:36 -0300

    first commit

---
+++ src/components/Nav/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <>
+            <ul>
+                <li>TFT</li>
+                <li>CHAMPIONS</li>
+                <li>TIER LIST X</li>
+                <li>BLOG</li>
+                <li><ion-icon name="ellipsis-horizontal-sharp"></ion-icon></li>
+            </ul>
+        </>
+    )         
+    
+}
+
+export default Nav;
